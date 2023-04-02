1e18a644c6350518723164949ea836dc29af4598
AJEET KUSHWAHA <51816732+ajeet-lab@users.noreply.github.com>
2023-04-02 18:21:38 +0530

    Add More category method

--- src/main/java/com/ajeet/blog/ajeetblog/servicesImpl/CategoryServiceImpl.java
+++ src/main/java/com/ajeet/blog/ajeetblog/servicesImpl/CategoryServiceImpl.java
@@ -1,12 +1,14 @@
 package com.ajeet.blog.ajeetblog.servicesImpl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ajeet.blog.ajeetblog.entites.Category;
+import com.ajeet.blog.ajeetblog.exceptions.ResourceNotFoundException;
 import com.ajeet.blog.ajeetblog.payloads.CategoryDto;
 import com.ajeet.blog.ajeetblog.repos.CateRepo;
 import com.ajeet.blog.ajeetblog.services.CategoryService;
@@ -29,22 +31,30 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public CategoryDto updateCatDto(CategoryDto catDto, int catId) {
-        throw new UnsupportedOperationException("Unimplemented method 'updateCatDto'");
+        Category category = this.cateRepo.findById(catId).orElseThrow(() -> new ResourceNotFoundException("Category ", "CategoryId ", catId));
+        category.setCategoryName(catDto.getCategoryName());
+        category.setCategoryDescription(catDto.getCategoryDescription());
+        Category updatedCat = this.cateRepo.save(category);
+        return this.modelMapper.map(updatedCat, CategoryDto.class);
     }
 
     @Override
     public CategoryDto getCatById(int catId) {
-        throw new UnsupportedOperationException("Unimplemented method 'getCatById'");
+        Category category = this.cateRepo.findById(catId).orElseThrow(() -> new ResourceNotFoundException("Category ", "CategoryId ", catId));
+        return this.modelMapper.map(category, CategoryDto.class);
     }
 
     @Override
     public List<CategoryDto> getAllCats() {
-        throw new UnsupportedOperationException("Unimplemented method 'getAllCats'");
+        List<Category> category = this.cateRepo.findAll();
+        List<CategoryDto> catDtos = category.stream().map(cate -> this.modelMapper.map(cate, CategoryDto.class)).collect(Collectors.toList());
+        return catDtos;
     }
 
     @Override
     public void deleteCatById(int catId) {
-        throw new UnsupportedOperationException("Unimplemented method 'deleteCatById'");
+        Category category = this.cateRepo.findById(catId).orElseThrow(() -> new ResourceNotFoundException("Category ", "CategoryId ", catId));
+        this.cateRepo.delete(category);
     }
     
 }
